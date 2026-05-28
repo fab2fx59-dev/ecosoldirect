276247033f43d0c4d2a0cc9c4a3912feb9f7c8f0
fab2fx59-dev <fab2fx59@gmail.com>
2026-05-28 16:55:56 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-H593VXW1K1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-H593VXW1K1');
-</script>
 
 'use client'
 
