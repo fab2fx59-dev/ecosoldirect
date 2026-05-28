8703f57b2a34bf87577d731035d0aac1c1c9ca03
fab2fx59-dev <fab2fx59@gmail.com>
2026-05-28 16:58:04 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,30 @@
-<html lang="fr">
-  <body>
-    {children}
+export const metadata = {
+  title: 'ECOSOL Direct',
+  description: 'Solutions photovoltaïques au sol pour particuliers, professionnels et agriculteurs.',
+}
 
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H593VXW1K1"></script>
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
 
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-H593VXW1K1"
+        />
 
-          gtag('config', 'G-H593VXW1K1');
-        `,
-      }}
-    />
-  </body>
-</html>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-H593VXW1K1');
+            `,
+          }}
+        />
+      </body>
+    </html>
+  )
+}
